273c7ad06f3788720981704a91c594d781713be7
Gopi P <pgopirmd@gmail.com>
2024-02-11 22:20:57 +0530

    new design changes are added

--- frontend_new/bobsco/src/components/card/index.js
+++ frontend_new/bobsco/src/components/card/index.js
@@ -1,28 +1,27 @@
 import React from "react";
 
-export default function Card() {
+export default function Card({ courseName, smallDesc, img }) {
   return (
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
       <a href="#">
-        <img class="rounded-t-lg" src="images/card-bg.jpg" alt="" />
+        <img className="rounded-t-lg h-52 w-full" src={img} alt="" />
       </a>
-      <div class="p-5">
+      <div className="p-5">
         <a href="#">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-            Noteworthy technology acquisitions 2021
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+            {courseName}
           </h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {smallDesc}
         </p>
         <a
           href="#"
-          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-800 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-blue-800"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-800 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-red-800 dark:hover:bg-red-700 dark:focus:ring-blue-800 mt-5"
         >
           Read more
           <svg
-            class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
--- frontend_new/bobsco/src/components/courses/index.js
+++ frontend_new/bobsco/src/components/courses/index.js
@@ -4,13 +4,28 @@ import Card from "../card";
 export default function Courses() {
   return (
     <section className="mx-auto max-w-10xl py-14 bg-gray-100 px-5">
-      <p className="capitalize text-3xl font-bold">
-        Stories from Virginia Tech News
-      </p>
+      <p className="capitalize text-3xl font-bold">Top Courses</p>
       <div className="grid grid-rows-1 grid-flow-row  md:grid-flow-col gap-4 mt-6">
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          courseName="Data Analysis"
+          smallDesc="Data analysis involves inspecting, cleansing, transforming, and modeling data to discover useful information, inform conclusions, and support decision-making. It's a critical process in various domains to extract insights and assess the performance of different strategies or processes."
+          img="images/course/DA.jpg"
+        />
+        <Card
+          courseName="Artificial Intelligence"
+          smallDesc="Artificial Intelligence (AI) encompasses the development of computer systems that can perform tasks typically requiring human intelligence, such as visual perception, speech recognition, decision-making, and language translation. It aims to simulate human cognitive processes through algorithms and machine learning models."
+          img="images/course/AI.webp"
+        />
+        <Card
+          courseName="Data Science"
+          smallDesc="Data Science is an interdisciplinary field that uses scientific methods, processes, algorithms, and systems to extract knowledge and insights from structured and unstructured data. It combines aspects of statistics, data analysis, machine learning, and their related methods to understand and analyze actual phenomena with data."
+          img="images/course/DS.jpg"
+        />
+        <Card
+          courseName="Data Engineering"
+          smallDesc="Data Engineering involves the design, construction, and maintenance of systems for collecting, storing, and analyzing data at scale. It focuses on the practical application of data collection and data processing frameworks, ensuring data is accessible and usable for analytics and machine learning tasks."
+          img="images/course/DE.jpg"
+        />
       </div>
     </section>
   );
