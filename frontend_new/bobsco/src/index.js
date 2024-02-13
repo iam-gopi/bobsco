8683a584e7e6557b24805406d3bbef5e537b720b
iam-gopi <pgopirmd@gmail.com>
2024-02-14 00:11:02 +0530

    some more changes

--- frontend_new/bobsco/src/index.js
+++ frontend_new/bobsco/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Courses from "./components/courses";
 import VedhikModel from "./components/vedhik-model";
@@ -25,11 +24,5 @@ const router = createBrowserRouter([
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    {/* <App /> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
