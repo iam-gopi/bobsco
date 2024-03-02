dcceee54ca8778bf0f2887f332543746c5d9513e
Gopi P <pgopirmd@gmail.com>
2024-03-02 22:27:52 +0530

    code corrections

--- backend_new/index.js
+++ backend_new/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDatabase = require("./repository/config/DBConnection");
 const eVedhikRoutes = require("./routes/Evedhik");
+const urls = require("./utils/UrlConstant");
 
 connectDatabase();
 
@@ -12,7 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/evedhik", eVedhikRoutes);
+app.use(urls.evedhik, eVedhikRoutes);
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "api working fine" });
+});
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Server Started at ${process.env.PORT}`);
