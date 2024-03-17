7943037687f059b9dca011b9d7dda8a88aa6ff80
Gopi P <pgopirmd@gmail.com>
2024-03-17 21:35:41 +0530

    Gallery included

--- frontend_new/bobsco/src/components/gallery/Gallery.js
+++ frontend_new/bobsco/src/components/gallery/Gallery.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { ImageGallery } from "react-image-grid-gallery";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  let images = [];
+  for (let index = 1; index <= 50; index++)
+    images.push({
+      src: `${process.env.PUBLIC_URL}/images/Gallery/${index}.jpeg`,
+    });
+
+  return (
+    <main className="mx-auto my-14 sm:container">
+      <ImageGallery
+        imagesInfoArray={images}
+        columnWidth={330}
+        gapSize={24}
+        className="rounded"
+      />
+    </main>
+  );
 };
 
 export default Gallery;
