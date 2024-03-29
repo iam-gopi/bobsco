561cf83bf0c3a23dd8d8bd69f110f8248d9f5fca
iam-gopi <pgopirmd@gmail.com>
2024-03-29 23:58:28 +0530

    payment integration changes for FE

--- backend_new/routes/Evedhik.js
+++ backend_new/routes/Evedhik.js
@@ -3,19 +3,19 @@ const router = express.Router();
 
 router.post("/register", (req, res, next) => {
   try {
-  const v = new VedhikSchema({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    phone: req.body.phone,
-    address: req.body.address,
-  });
+    const v = new VedhikSchema({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      address: req.body.address,
+    });
 
-  const result = v.save();
-  res.send("Post API");
-}catch(Error er) {
-  
-}
+    const result = v.save();
+    res.send("Post API");
+  } catch (er) {
+    console.error(er);
+  }
 });
 
 module.exports = eVedhikRoutes = router;
